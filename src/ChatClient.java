3a62092b7a6dbd84b29f12523fa95d63645ed8fa
ViperFangs <77452390+ViperFangs@users.noreply.github.com>
2023-04-03 22:15:06 -0700

    Implement method setUpGUI()

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,7 +12,10 @@ import java.net.UnknownHostException;
 public class ChatClient {
     private PrintWriter serverWriter;
     private Socket serverConnection;
+    private JTextField txtOutgoing;
+    private JTextArea messageDisplay;
     public ChatClient() {
+        this.setUpGUI();
         this.setUpNetwork();
         this.setUpThread();
     }
@@ -22,7 +29,7 @@ public class ChatClient {
             error.printStackTrace();
         }
 
-        Runnable serverListener = new ServerHandler(serverReader);
+        Runnable serverListener = new ServerHandler(serverReader, messageDisplay);
         Thread serverListenerThread = new Thread(serverListener);
         serverListenerThread.start();
     }
@@ -45,5 +52,45 @@ public class ChatClient {
         }
 
         System.out.println("Connected to Server ...");
+    }
+
+    private void setUpGUI() {
+        JFrame frame = new JFrame("Chat Client");
+        JPanel mainPanel = new JPanel();
+
+        this.messageDisplay = new JTextArea(15, 50);
+        this.messageDisplay.setLineWrap(true);
+        this.messageDisplay.setWrapStyleWord(true);
+        this.messageDisplay.setEditable(false);
+
+        JScrollPane scrollBar = new JScrollPane(this.messageDisplay);
+        scrollBar.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollBar.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        this.txtOutgoing = new JTextField(20);
+
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+
+        mainPanel.add(scrollBar);
+        mainPanel.add(txtOutgoing);
+        mainPanel.add(sendButton);
+
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+
+        frame.setSize(640, 480);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private class SendButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            serverWriter.println(txtOutgoing.getText());
+            serverWriter.flush();
+
+            txtOutgoing.setText("");
+            txtOutgoing.requestFocus();
+        }
     }
 }
