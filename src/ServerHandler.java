ae708844af660caaf23cced2dc3f0ad5c3f2f8b6
ViperFangs <77452390+ViperFangs@users.noreply.github.com>
2023-04-03 22:14:49 -0700

    Add text to GUI update

--- src/ServerHandler.java
+++ src/ServerHandler.java
@@ -1,10 +1,13 @@
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 
 public class ServerHandler implements Runnable {
     private BufferedReader serverReader;
-    public ServerHandler(BufferedReader serverReader) {
+    private JTextArea textDisplay;
+    public ServerHandler(BufferedReader serverReader, JTextArea messageDisplay) {
         this.serverReader = serverReader;
+        this.textDisplay = messageDisplay;
     }
 
     @Override
@@ -15,6 +18,7 @@ public class ServerHandler implements Runnable {
         try {
             while ((message = this.serverReader.readLine()) != null) {
                 System.out.println("Received: " + message);
+                this.textDisplay.append(message + "\n");
             }
         } catch (IOException error) {
             error.printStackTrace();
